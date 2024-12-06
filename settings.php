7abc879d45416e05c73f3330fd07e751e23d1b36
egyptian-ghost <153223375+egyptian-ghost@users.noreply.github.com>
2024-12-06 19:56:56 +0200

    Create settings.php

---
+++ settings.php
@@ -0,0 +1,15 @@
+<?php
+
+// Telegram Settings
+$telegramToken = 'YOUR_BOT_API_TOKEN'; // Put your token here
+$telegramChatId = 'YOUR_CHAT_ID'; // Put your chat ID here
+$folder="results/";
+// File Settings
+$userDic = 'user.txt';
+$passDic = 'password.txt';
+$urlDic =  'urls.txt';
+$successLog = $folder.'recheck.txt';
+$failLog = $folder.'fail.txt';
+$wafLog = $folder.'waf.txt';
+
+?>
